b07c936aad30ba67d9e55eee16c732bc449a1d03
Bruce <dongsheng.zhang@skyinno.com>
2016-12-07 14:57:32 +0800

    调整license及maven引用版本

--- src/main/java/com/alibaba/fastjson/serializer/MapSerializer.java
+++ src/main/java/com/alibaba/fastjson/serializer/MapSerializer.java
@@ -1,27 +1,29 @@
-/**
- * Copyright (C) 2014-2015 The Skfiy Open Association.
- *
+/*-
+ * ========================LICENSE_START=================================
+ * Smartconfig Maven Plugin
+ * *
+ * Copyright (C) 2016 BruceZhang
+ * *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * =========================LICENSE_END==================================
  */
+
 package com.alibaba.fastjson.serializer;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
 import com.alibaba.fastjson.JSON;
 
@@ -32,7 +34,7 @@ public class MapSerializer implements ObjectSerializer {
 
     public static MapSerializer instance = new MapSerializer();
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @SuppressWarnings({ "rawtypes" })
     public void write(JSONSerializer serializer, Object object, Object fieldName, Type fieldType, int features) throws IOException {
         SerializeWriter out = serializer.getWriter();
 
